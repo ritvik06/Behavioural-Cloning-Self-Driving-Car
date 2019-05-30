f49bbbd0699724bed11617af2683b41a5f5ea8b3
ritvik06 <ritvikvij06@gmail.com>
2019-05-30 11:48:05 +0530

    Model made with quantized weights to check efficiency

--- weights.py
+++ weights.py
@@ -1,6 +1,10 @@
 import tensorflow as tf
 from keras.models import load_model
+import numpy as np
+
+a = []
 
 model = load_model('./model.h5')
-model.get_weights()
-
+a=model.get_weights()
+A = np.array(a)
+np.save('weights.npy',A)
